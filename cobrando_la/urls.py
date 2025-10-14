68233b780b588a05ed256103160148f821eee539
Fernando Emilio Diaz Lopez <fernandoemiliolopez13@gmail.com>
2025-10-14 16:22:47 +0000

    ✨ Add About page and update URL routing for improved navigation

--- cobrando_la/urls.py
+++ cobrando_la/urls.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from bank_details.views import public_profile
-from home.views import index
+from home.views import index, about
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("", index, name="home"),  # Landing Page
+    path("about/", about, name="about"),  # About Page
     path("dashboard/", include("bank_details.urls")),  # Dashboard
     path("u/<slug:public_slug>/", public_profile, name="public_profile"),
 ]
--- home/views.py
+++ home/views.py
@@ -5,3 +5,7 @@ def index(request):
     return render(request, 'home/index.html', {
         'user': request.user
     })
+
+
+def about(request):
+    return render(request, 'home/about.html')
